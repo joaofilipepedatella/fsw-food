05f6eb0b1791742a7a620b3f6372f6c97e9294bf
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-04-30 18:29:28 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,18 @@ export default function Home() {
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
     </>
   );
 }
