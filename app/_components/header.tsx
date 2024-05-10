677377e0c8e592ac6bb4617d65ce60f6882fe6d1
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2024-05-10 12:38:34 -0300

    feat: add asChild on sheet trigger on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -39,7 +39,7 @@ const Header = () => {
         </Link>
       </div>
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger asChild>
           <Button
             size="icon"
             variant="outline"
